5547b98809793b0cf73d797e5ab607864c74a5b5
ComLarsic <larsjanjensen@gmail.com>
2023-10-27 10:44:03 +0200

    Add webclient project

---
+++ webclient/src/lib/index.ts
@@ -0,0 +1,5 @@
+export const greeting: {
+    name: string
+} = {
+    name: 'world'
+};
---
+++ webclient/svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// Setup the static adapters
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
