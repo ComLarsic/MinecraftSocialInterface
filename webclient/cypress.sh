595ad6b417422711aa2271b0d5299e01bae21403
ComLarsic <larsjanjensen@gmail.com>
2024-01-19 11:32:27 +0100

    eula

--- webclient/cypress.sh
+++ webclient/cypress.sh
@@ -1,9 +1,9 @@
 # Create the fabric/run directory if it doesn't exist
-mkdir -p ../fabric/run
+mkdir -p fabric/run
 # Add the eula file
 echo "eula=true" > ../fabric/run/eula.txt
 # Start the server
-../gradlew fabric:runServer > LOG.txt &
+./gradlew fabric:runServer > LOG.txt &
 # Wait for the server to start
 echo "Waiting for server"
 while ! netstat -tna | grep 'LISTEN\>' | grep -q ':8080\>'; do
@@ -13,4 +13,5 @@ while ! netstat -tna | grep 'LISTEN\>' | grep -q ':8080\>'; do
 done
 # Start the cypress tests
 echo "Webclient is up"
+cd webclient
 npm run cypress
